121b7fd2c59d5da0149dfd5038293feaf6c4b34f
remakefactory <215389873+remakefactory@users.noreply.github.com>
2026-01-29 21:20:20 +0800

    equals and hashCode

--- src/main/java/com/ref/moremorelang/lang/ControllableTranslatableContents.java
+++ src/main/java/com/ref/moremorelang/lang/ControllableTranslatableContents.java
@@ -36,4 +36,20 @@ public class ControllableTranslatableContents extends TranslatableContents {
       this.decomposedParts = ImmutableList.of(FormattedText.of(formatTemplate));
     }
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ControllableTranslatableContents that) || !super.equals(o)) {
+      return false;
+    }
+    return Objects.equals(this.targetLanguage, that.targetLanguage);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), this.targetLanguage);
+  }
 }
--- src/main/java/com/ref/moremorelang/mixin/TranslatableContentsMixin.java
+++ src/main/java/com/ref/moremorelang/mixin/TranslatableContentsMixin.java
@@ -1,19 +1,34 @@
 package com.ref.moremorelang.mixin;
 
 import com.ref.moremorelang.lang.ControllableLanguageComponent;
+import java.util.Arrays;
+import java.util.Objects;
+import javax.annotation.Nullable;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TranslatableContents.class)
 @OnlyIn(Dist.CLIENT)
-public class TranslatableContentsMixin implements ControllableLanguageComponent {
+public abstract class TranslatableContentsMixin implements ControllableLanguageComponent {
+  @Shadow
+  public abstract String getKey();
+
+  @Shadow
+  @Nullable
+  public abstract String getFallback();
+
+  @Shadow
+  public abstract Object[] getArgs();
+
   @Unique private Language moreMoreLang$targetLanguage;
 
   @Override
@@ -36,5 +51,39 @@ public class TranslatableContentsMixin implements ControllableLanguageComponent 
       return this.moreMoreLang$targetLanguage;
     }
     return originalLanguage;
+  }
+
+  /**
+   * @author remakefactory
+   * @reason Overwrite equals to include the custom targetLanguage field, preventing incorrect cache
+   *     hits.
+   */
+  @Overwrite
+  public boolean equals(Object pOther) {
+    if (this == pOther) {
+      return true;
+    }
+    if (!(pOther instanceof TranslatableContents that)) {
+      return false;
+    }
+    TranslatableContentsMixin thatMixin = (TranslatableContentsMixin) (Object) that;
+    return Objects.equals(this.getKey(), that.getKey())
+        && Objects.equals(this.getFallback(), that.getFallback())
+        && Arrays.equals(this.getArgs(), that.getArgs())
+        && Objects.equals(this.moreMoreLang$targetLanguage, thatMixin.moreMoreLang$targetLanguage);
+  }
+
+  /**
+   * @author remakefactory
+   * @reason Overwrite hashCode to include the custom targetLanguage field, ensuring consistency
+   *     with the modified equals method.
+   */
+  @Overwrite
+  public int hashCode() {
+    int result = Objects.hashCode(this.getKey());
+    result = 31 * result + Objects.hashCode(this.getFallback());
+    result = 31 * result + Arrays.hashCode(this.getArgs());
+    result = 31 * result + Objects.hashCode(this.moreMoreLang$targetLanguage);
+    return result;
   }
 }
